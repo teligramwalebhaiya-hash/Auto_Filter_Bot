a7933d45dacac53cd08f922d5adc1b4f699075c5
teligramwalebhaiya-hash <teligramwalebhaiya@gmail.com>
2025-12-11 12:23:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -129,7 +129,7 @@ UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/graduate_movies')
 # ============================
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '7859995064').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '').split()]
+PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '7859995064').split()]
 
 # ============================
 # Miscellaneous Configuration
